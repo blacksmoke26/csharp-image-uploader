0c86b6cbfe0f015384781486c9b5c36b37a906d8
Junaid Atari <mj.atari@gmail.com>
2024-12-31 18:56:45 +0500

    create user control

---
+++ ImageShare/UserControls/ImageThumbControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using ImageShare.Helpers;
+
+namespace ImageShare.UserControls;
+
+public partial class ImageThumbControl {
+  private static readonly RoutedEvent RemoveClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(RemoveClick), RoutingStrategy.Bubble,
+    typeof(RoutedEventHandler), typeof(ImageThumbControl)
+  );
+
+  public event RoutedEventHandler RemoveClick {
+    add => AddHandler(RemoveClickEvent, value);
+    remove => RemoveHandler(RemoveClickEvent, value);
+  }
+
+  private static readonly RoutedEvent EditClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(EditClick), RoutingStrategy.Bubble,
+    typeof(RoutedEventHandler), typeof(ImageThumbControl)
+  );
+
+  public event RoutedEventHandler EditClick {
+    add => AddHandler(EditClickEvent, value);
+    remove => RemoveHandler(EditClickEvent, value);
+  }
+
+  public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
+    nameof(ImageSource), typeof(ImageThumb), typeof(ImageThumbControl),
+    new PropertyMetadata(default(ImageThumb))
+  );
+
+  public ImageThumb ImageSource {
+    get => (ImageThumb)GetValue(ImageSourceProperty);
+    set => SetValue(ImageSourceProperty, value);
+  }
+
+  public ImageThumbControl() {
+    InitializeComponent();
+  }
+
+  private void ImageRemoveButton_OnClick(object sender, RoutedEventArgs e) {
+    // Create a RoutedEventArgs instance.
+    RoutedEventArgs routedEventArgs = new(routedEvent: RemoveClickEvent);
+
+    // Raise the event, which will bubble up through the element tree.
+    RaiseEvent(routedEventArgs);
+  }
+}
