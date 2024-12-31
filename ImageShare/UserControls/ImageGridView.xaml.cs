27d24595b5fe96148c4c1802e3c2b3721fee90c3
Junaid Atari <mj.atari@gmail.com>
2024-12-31 18:59:47 +0500

    Create minimal listview

--- ImageShare/UserControls/ImageGridView.xaml.cs
+++ ImageShare/UserControls/ImageGridView.xaml.cs
@@ -1,18 +1,32 @@
 using System.Windows;
 using System.Windows.Controls;
+using ImageShare.Helpers;
 
 namespace ImageShare.UserControls;
 
 public partial class ImageGridView : UserControl {
   public static readonly DependencyProperty ImagesListProperty = DependencyProperty.Register(
-    nameof(ImagesList), typeof(List<string>), typeof(ImageGridView), new PropertyMetadata(default(List<string>)));
+    nameof(ImagesList), typeof(List<ImageThumb>), typeof(ImageGridView),
+    new PropertyMetadata(null, ImagesListChangedCallback));
 
-  public List<string> ImagesList {
-    get => (List<string>)GetValue(ImagesListProperty);
+  private static void ImagesListChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+    if (d is not ImageGridView control || e.NewValue == null) return;
+    
+    var list = (List<ImageThumb>)e.NewValue;
+    control.ThumbsListView.ItemsSource = list;
+  }
+
+  public List<ImageThumb> ImagesList {
+    get => (List<ImageThumb>)GetValue(ImagesListProperty);
     set => SetValue(ImagesListProperty, value);
   }
 
   public ImageGridView() {
     InitializeComponent();
+    ThumbsListView.ItemsSource = ImagesList;
+  }
+
+  private void ImageThumbControl_OnRemoveClick(object sender, RoutedEventArgs e) {
+    Console.WriteLine("____CLICKED____");
   }
 }
