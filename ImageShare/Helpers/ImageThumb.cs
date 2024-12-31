5808a595a39bc1e3a2b83312fe4de8d1110b0254
Junaid Atari <mj.atari@gmail.com>
2024-12-31 18:57:27 +0500

    create method to generate thumb image

--- ImageShare/Helpers/ImageThumb.cs
+++ ImageShare/Helpers/ImageThumb.cs
@@ -1,28 +1,39 @@
 using System.IO;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Metadata;
+using SixLabors.ImageSharp.Processing;
 
 namespace ImageShare.Helpers;
 
 public class ImageThumb {
-  private string _uid = Guid.NewGuid().ToString();
+  public string Source { get; init; }
+
   public string Title { get; set; } = string.Empty;
   public bool IsProcessed { get; private set; }
-  public string Source { get; init; }
   public string HostedUrl { get; private set; } = string.Empty;
+  public string ThumbnailPath { get; private set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
 
   public int Width { get; set; }
   public int Height { get; set; }
-  public string Description { get; set; } = string.Empty;
 
   public ImageThumb(string source) {
     Source = source;
     Initialize();
   }
 
+  public string GenerateThumbnail(int width = 120, int height = 120) {
+    var outPath = Path.GetTempFileName();
+
+    using var image = Image.Load(Source);
+    image.Mutate(x => x.Resize(width, height));
+
+    image.SaveAsJpeg(outPath);
+    return outPath;
+  }
+
   private void Initialize() {
     if (!ImageHelper.IsImageExt(Source)) return;
-    
+
     ImageInfo imageInfo;
     try {
       imageInfo = Image.Identify(Source);
@@ -37,6 +48,7 @@ public class ImageThumb {
     Width = imageInfo.Width;
     Height = imageInfo.Height;
     Title = Path.GetFileNameWithoutExtension(Source);
+    ThumbnailPath = GenerateThumbnail(110, 110);
     IsProcessed = true;
   }
 }
