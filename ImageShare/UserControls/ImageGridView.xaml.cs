63e32eba7b9126613411d999b290283e1efb453e
Junaid Atari <mj.atari@gmail.com>
2025-01-04 20:35:48 +0500

    improve ui, implement event triggers, change list to binding-list

--- ImageShare/UserControls/ImageGridView.xaml.cs
+++ ImageShare/UserControls/ImageGridView.xaml.cs
@@ -1,26 +1,47 @@
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Dumpify;
 using ImageShare.Helpers;
 
 namespace ImageShare.UserControls;
 
 public partial class ImageGridView : UserControl {
   public static readonly DependencyProperty ImagesListProperty = DependencyProperty.Register(
-    nameof(ImagesList), typeof(List<ImageThumb>), typeof(ImageGridView),
+    nameof(ImagesList), typeof(BindingList<ImageThumb>), typeof(ImageGridView),
     new PropertyMetadata(null, ImagesListChangedCallback));
 
   private static void ImagesListChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e) {
     if (d is not ImageGridView control || e.NewValue == null) return;
-    
-    var list = (List<ImageThumb>)e.NewValue;
+
+    var list = (BindingList<ImageThumb>)e.NewValue;
     control.ThumbsListView.ItemsSource = list;
-    Console.WriteLine(list.Count);
   }
 
-  public List<ImageThumb> ImagesList {
-    get => (List<ImageThumb>)GetValue(ImagesListProperty);
+  public BindingList<ImageThumb> ImagesList {
+    get => (BindingList<ImageThumb>)GetValue(ImagesListProperty);
     set => SetValue(ImagesListProperty, value);
+  }
+
+  private static readonly RoutedEvent RemoveClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(RemoveClick), RoutingStrategy.Bubble,
+    typeof(RoutedEventHandler), typeof(Button)
+  );
+
+  public event RoutedEventHandler RemoveClick {
+    add => AddHandler(RemoveClickEvent, value);
+    remove => RemoveHandler(RemoveClickEvent, value);
+  }
+
+  private static readonly RoutedEvent EditClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(EditClick), RoutingStrategy.Bubble,
+    typeof(RoutedEventHandler), typeof(Button)
+  );
+
+  public event RoutedEventHandler EditClick {
+    add => AddHandler(EditClickEvent, value);
+    remove => RemoveHandler(EditClickEvent, value);
   }
 
   public ImageGridView() {
@@ -28,15 +49,14 @@ public partial class ImageGridView : UserControl {
     ThumbsListView.ItemsSource = ImagesList;
   }
 
-  private void ImageThumbControl_OnRemoveClick(object sender, RoutedEventArgs e) {
-    Console.WriteLine("____CLICKED____");
-  }
-
   private void ImageEdit_OnMouseDown(object sender, MouseButtonEventArgs e) {
-    Console.WriteLine("____CLICKED____");
+    RaiseEvent(new RoutedEventArgs(EditClickEvent) {
+      Source = (sender as Border)?.DataContext
+    });
   }
 
   private void ImageRemove_OnMouseDown(object sender, MouseButtonEventArgs e) {
-    Console.WriteLine("____CLICKED____");
+    ImagesList.Remove((ImageThumb)(sender as Border)?.DataContext!);
+    RaiseEvent(new RoutedEventArgs(RemoveClickEvent));
   }
 }
