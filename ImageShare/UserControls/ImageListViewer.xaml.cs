99964b557b3cd23f5bbc59bddc4a4d321ba555a0
Junaid Atari <mj.atari@gmail.com>
2025-01-08 14:14:17 +0500

    create events to invoke list-view-item events

--- ImageShare/UserControls/ImageListViewer.xaml.cs
+++ ImageShare/UserControls/ImageListViewer.xaml.cs
@@ -42,6 +42,26 @@ public partial class ImageListViewer {
     remove => RemoveHandler(EditClickEvent, value);
   }
 
+  private static readonly RoutedEvent ViewErrorClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(ViewErrorClick), RoutingStrategy.Bubble,
+    typeof(RoutedEventHandler), typeof(ImageListViewer)
+  );
+
+  public event RoutedEventHandler ViewErrorClick {
+    add => AddHandler(ViewErrorClickEvent, value);
+    remove => RemoveHandler(ViewErrorClickEvent, value);
+  }
+
+  private static readonly RoutedEvent ViewDetailClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(ViewDetailClick), RoutingStrategy.Bubble,
+    typeof(RoutedEventHandler), typeof(ImageListViewer)
+  );
+
+  public event RoutedEventHandler ViewDetailClick {
+    add => AddHandler(ViewDetailClickEvent, value);
+    remove => RemoveHandler(ViewDetailClickEvent, value);
+  }
+
   public ImageListViewer() {
     InitializeComponent();
     ThumbsListView.ItemsSource = ImagesList;
@@ -59,5 +79,17 @@ public partial class ImageListViewer {
     var obj = (ImageListViewItem)sender;
     ImagesList.Remove(obj.ImageItem);
     RaiseEvent(new RoutedEventArgs(RemoveClickEvent));
+  }
+
+  private void ImageListViewItem_OnViewDetailsClick(object sender, RoutedEventArgs e) {
+    RaiseEvent(new RoutedEventArgs(ViewDetailClickEvent) {
+      Source = ((ImageListViewItem)sender).ImageItem
+    });
+  }
+
+  private void ImageListViewItem_OnViewErrorClick(object sender, RoutedEventArgs e) {
+    RaiseEvent(new RoutedEventArgs(ViewErrorClickEvent) {
+      Source = ((ImageListViewItem)sender).ImageItem
+    });
   }
 }
