b6c98713180fd59912d89da6ee109d124cd9969d
Junaid Atari <mj.atari@gmail.com>
2025-01-08 17:11:27 +0500

    add application icon

--- ImageShare/App.xaml.cs
+++ ImageShare/App.xaml.cs
@@ -6,6 +6,7 @@ namespace ImageShare;
 
 /// <summary>
 /// Interaction logic for App.xaml
+/// https://icon-icons.com/icon/upload-pictures/93667
 /// </summary>
 public partial class App : Application {
 }
