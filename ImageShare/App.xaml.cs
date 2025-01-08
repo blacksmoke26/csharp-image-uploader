92304b305df6e2293b8e74f55b0cf530ae5e834c
Junaid Atari <mj.atari@gmail.com>
2025-01-08 20:59:27 +0500

    implement functionality to load .env file

--- ImageShare/App.xaml.cs
+++ ImageShare/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using PixPost.Helpers;
 
 namespace PixPost;
 
@@ -8,5 +6,8 @@ namespace PixPost;
 /// Interaction logic for App.xaml
 /// https://icon-icons.com/icon/upload-pictures/93667
 /// </summary>
-public partial class App : Application {
+public partial class App {
+  public App() {
+    ConfigLoader.LoadExisting();
+  }
 }
