035ea40c2b01f85946f06fd42f65805eb57fd0ee
Junaid Atari <mj.atari@gmail.com>
2025-01-09 17:12:44 +0500

    fix issue whereby link won't open in browser

--- ImageShare/Dialogs/SettingsDialog.xaml.cs
+++ ImageShare/Dialogs/SettingsDialog.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Input;
 
 namespace PixPost.Dialogs;
@@ -6,6 +6,7 @@ namespace PixPost.Dialogs;
 public partial class SettingsDialog {
   public SettingsDialog() {
     InitializeComponent();
+    EndpointTextBox.Focus();
   }
 
   private void CloseTextBlock_OnMouseDown(object sender, MouseButtonEventArgs e) {
@@ -14,5 +15,12 @@ public partial class SettingsDialog {
 
   private void MainWindow_OnMouseDown(object _, MouseButtonEventArgs e) {
     if (e is { LeftButton: MouseButtonState.Pressed, ButtonState: MouseButtonState.Pressed }) DragMove();
+  }
+
+  private void WebUrlClick_OnMouseDown(object sender, MouseButtonEventArgs e) {
+    Process.Start(new ProcessStartInfo {
+      FileName = "https://api.imgbb.com/",
+      UseShellExecute = true
+    });
   }
 }
