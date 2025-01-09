98018f23c5eaa9ae65f3aaa322dd9acfb40a51ca
Junaid Atari <mj.atari@gmail.com>
2025-01-09 17:13:25 +0500

    create utility class

---
+++ ImageShare/Helpers/DialogHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using PixPost.Dialogs;
+
+namespace PixPost.Helpers;
+
+public static class DialogHelper {
+  public static void ShowErrorDialog(Window window, string description, string heading, Action<SimpleDialog>? callback) {
+    var dialog = new SimpleDialog() {
+      HeadingText = heading,
+      DescriptionText = description,
+      Owner = window,
+      WindowStartupLocation = WindowStartupLocation.CenterOwner,
+    };
+
+    callback?.Invoke(dialog);
+    dialog.ShowDialog();
+  }
+}
