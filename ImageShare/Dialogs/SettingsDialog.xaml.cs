d23f0227c83fc99a5ad8a9ae64b98e1ad335b0f0
Junaid Atari <mj.atari@gmail.com>
2025-01-09 14:44:13 +0500

    create settings dialog

---
+++ ImageShare/Dialogs/SettingsDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PixPost.Dialogs;
+
+public partial class SettingsDialog {
+  public SettingsDialog() {
+    InitializeComponent();
+  }
+
+  private void CloseTextBlock_OnMouseDown(object sender, MouseButtonEventArgs e) {
+    Close();
+  }
+
+  private void MainWindow_OnMouseDown(object _, MouseButtonEventArgs e) {
+    if (e is { LeftButton: MouseButtonState.Pressed, ButtonState: MouseButtonState.Pressed }) DragMove();
+  }
+}
