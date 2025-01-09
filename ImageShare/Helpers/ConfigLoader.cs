65b713f803a54682d0a15fb2e5a83bfc873b9899
Junaid Atari <mj.atari@gmail.com>
2025-01-09 19:44:58 +0500

    add method, make class static

--- ImageShare/Helpers/ConfigLoader.cs
+++ ImageShare/Helpers/ConfigLoader.cs
@@ -3,13 +3,24 @@ using DotNetEnv;
 
 namespace PixPost.Helpers;
 
-public class ConfigLoader : Env {
+public static class ConfigLoader {
+  private const string ImgBbEndpoint = "IMGBB_ENDPOINT";
+  private const string ImgBbApiKey = "IMGBB_API_KEY";
+
   public static void LoadExisting() {
     var envFilePath = Path.Join(Environment.CurrentDirectory, ".env");
     if (!File.Exists(envFilePath)) {
       File.WriteAllText(envFilePath, ResourceLoader.ResourceList.GetInitialEnv());
     }
 
-    Load(envFilePath);
+    Env.Load(envFilePath);
+  }
+
+  public static string GetEndpoint() {
+    return Env.GetString(ImgBbEndpoint).Trim();
+  }
+
+  public static string GetApiKey() {
+    return Env.GetString(ImgBbApiKey).Trim();
   }
 }
