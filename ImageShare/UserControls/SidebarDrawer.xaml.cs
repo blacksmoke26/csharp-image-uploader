4ee51843812e523ef035cba6eca3337b6ba37863
Junaid Atari <mj.atari@gmail.com>
2025-01-12 23:50:39 +0500

    make menu items dynamic with event binding

--- ImageShare/UserControls/SidebarDrawer.xaml.cs
+++ ImageShare/UserControls/SidebarDrawer.xaml.cs
@@ -1,56 +1,54 @@
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Input;
 
 namespace PixPost.UserControls;
 
+public class SidebarMenuItem {
+  public delegate void ItemClicked(SidebarMenuItem item);
+
+  public required string Id { get; set; } = string.Empty;
+  public required string Label { get; set; } = string.Empty;
+  public required string Icon { get; set; } = string.Empty;
+  public bool HideDrawer { get; set; } = true;
+
+  public event ItemClicked? ItemClick;
+
+  public void OnItemClick() => ItemClick?.Invoke(this);
+}
+
 public partial class SidebarDrawer {
-  private static readonly RoutedEvent DrawerCloseEvent = EventManager.RegisterRoutedEvent(
-    nameof(DrawerClose), RoutingStrategy.Bubble,
+  public static readonly DependencyProperty MenuItemsProperty = DependencyProperty.Register(
+    nameof(MenuItems), typeof(ObservableCollection<SidebarMenuItem>), typeof(SidebarDrawer),
+    new PropertyMetadata(new ObservableCollection<SidebarMenuItem>(), (o, args) => {
+      var obj = (SidebarDrawer)o;
+      obj.SidebarMenu.ItemsSource = (ObservableCollection<SidebarMenuItem>)args.NewValue;
+    }));
+
+  public ObservableCollection<SidebarMenuItem> MenuItems {
+    get => (ObservableCollection<SidebarMenuItem>)GetValue(MenuItemsProperty);
+    set => SetValue(MenuItemsProperty, value);
+  }
+
+  private static readonly RoutedEvent ItemClickEvent = EventManager.RegisterRoutedEvent(
+    nameof(ItemClick), RoutingStrategy.Bubble,
     typeof(RoutedEventHandler), typeof(SidebarDrawer)
   );
 
-  public event RoutedEventHandler DrawerClose {
-    add => AddHandler(DrawerCloseEvent, value);
-    remove => RemoveHandler(DrawerCloseEvent, value);
+  public event RoutedEventHandler ItemClick {
+    add => AddHandler(ItemClickEvent, value);
+    remove => RemoveHandler(ItemClickEvent, value);
   }
 
   private void DrawerOutside_OnMouseDown(object sender, RoutedEventArgs e) {
-    RaiseEvent(new RoutedEventArgs(routedEvent: DrawerCloseEvent));
+    foreach (var item in SidebarMenu.Items) {
+      var itm = (SidebarMenuItem)item;
+      if (itm.Id == "Close")
+        itm.OnItemClick();
+    }
   }
-
-  private static readonly RoutedEvent ConfigurationClickEvent = EventManager.RegisterRoutedEvent(
-    nameof(ConfigurationClick), RoutingStrategy.Bubble,
-    typeof(RoutedEventHandler), typeof(SidebarDrawer)
-  );
-
-  public event RoutedEventHandler ConfigurationClick {
-    add => AddHandler(ConfigurationClickEvent, value);
-    remove => RemoveHandler(ConfigurationClickEvent, value);
-  }
-
-  private void ConfigurationMenuItem_OnClick(object sender, RoutedEventArgs e) {
-    RaiseEvent(new RoutedEventArgs(routedEvent: ConfigurationClickEvent));
-  }
-
-  private static readonly RoutedEvent HistoryClickEvent = EventManager.RegisterRoutedEvent(
-    nameof(HistoryClick), RoutingStrategy.Bubble,
-    typeof(RoutedEventHandler), typeof(SidebarDrawer)
-  );
-
-  public event RoutedEventHandler HistoryClick {
-    add => AddHandler(HistoryClickEvent, value);
-    remove => RemoveHandler(HistoryClickEvent, value);
-  }
-
-  private void HistoryMenuItem_OnClick(object sender, RoutedEventArgs e) {
-    RaiseEvent(new RoutedEventArgs(routedEvent: HistoryClickEvent));
-  }
-
+  
   public SidebarDrawer() {
     InitializeComponent();
-  }
-
-  private void ExitMenuItem_OnMouseDown(object sender, MouseButtonEventArgs e) {
-    RaiseEvent(new RoutedEventArgs(routedEvent: DrawerCloseEvent));
+    SidebarMenu.ItemsSource = MenuItems;
   }
 }
