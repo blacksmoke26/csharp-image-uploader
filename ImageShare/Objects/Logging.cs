22d9215c88796922580b27d171e15af2bc9aa2a4
Junaid Atari <mj.atari@gmail.com>
2025-01-12 23:48:12 +0500

    add new fields

--- ImageShare/Objects/Logging.cs
+++ ImageShare/Objects/Logging.cs
@@ -8,9 +8,12 @@ public abstract class Logging {
   }
 
   public class LogItem {
-    public string Uuid { get; private set; } = string.Empty;
+    public string Id { get; private set; } = string.Empty;
     public string Heading { get; set; } = string.Empty;
     public string Remarks { get; set; } = string.Empty;
+    public object OldValue { get; set; } = string.Empty;
+    public object NewValue { get; set; } = string.Empty;
+    public object? Meta { get; set; } = null;
     public LogItemStatus Status { get; set; } = LogItemStatus.Trace;
     public ImageThumb? ImageItem { get; set; }
     public DateTime CreatedAt { get; private set; } = DateTime.Now;
