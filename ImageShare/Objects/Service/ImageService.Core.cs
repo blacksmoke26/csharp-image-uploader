0f0e6589f02d177f1503a5195cbc48a1ce85b138
Junaid Atari <mj.atari@gmail.com>
2025-01-13 22:38:51 +0500

    change method signature

--- ImageShare/Objects/Service/ImageService.Core.cs
+++ ImageShare/Objects/Service/ImageService.Core.cs
@@ -1,3 +1,8 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2024-2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+using System.Text.RegularExpressions;
 using PixPost.Helpers;
 using PixPost.Objects.Service.Interfaces;
 
@@ -34,7 +39,9 @@ public partial class ImageService(ImageSchema schema) : IBaseImageService {
   public string GetVariablePrefix() => GetSchema().VariablePrefix;
 
   /// <inheritdoc/>
-  public SchemaSpecs.Menu GetMenu() => GetSchema().Menu;
+  public (string Caption, string Icon) GetMenu() {
+    return (GetSchema().Menu.Text, Regex.Unescape(GetSchema().Menu.Icon));
+  }
 
   /// <inheritdoc/>
   public Dictionary<string, string> GetExpirations() {
