429676488618fbbe3058919d3de45f064b95c746
Junaid Atari <mj.atari@gmail.com>
2025-01-13 20:37:01 +0500

    rename methods

--- ImageShare/Helpers/SchemaHelper.cs
+++ ImageShare/Helpers/SchemaHelper.cs
@@ -14,7 +14,7 @@ public static class SchemaHelper {
   /// </summary>
   /// <param name="schemaJson">The JSON text</param>
   /// <returns>Parsed JSON schema object</returns>
-  public static JSchema ParseSchema(string schemaJson) {
+  public static JSchema Parse(string schemaJson) {
     return JSchema.Parse(schemaJson);
   }
   
@@ -22,22 +22,22 @@ public static class SchemaHelper {
   /// Determines whatever the JSON (text) is valid against the Schema JSON
   /// </summary>
   /// <param name="schema">The schema to test with</param>
-  /// <param name="targetJson">The target json text</param>
+  /// <param name="targetJson">The target JSON text</param>
   /// <param name="errors">When this method returns, contains any error messages generated while validating.</param>
   /// <returns>True when no errors, false otherwise</returns>
-  public static bool ValidateText(string schema, string targetJson, out IList<string> errors) {
+  public static bool TryParse(string schema, string targetJson, out IList<string> errors) {
     return JObject.Parse(targetJson)
-      .IsValid(ParseSchema(schema), out errors);
+      .IsValid(Parse(schema), out errors);
   }
 
   /// <summary>
   /// Determines whatever the JSON (text) is valid against the Schema JSON
   /// </summary>
   /// <param name="schema">The schema to test with</param>
-  /// <param name="targetJson">The target json text</param>
+  /// <param name="targetJson">The target JSON text</param>
   /// <param name="errors">When this method returns, contains any error messages generated while validating.</param>
   /// <returns>True when no errors, false otherwise</returns>
-  public static bool ValidateText(JSchema schema, string targetJson, out IList<string> errors) {
+  public static bool TryParse(JSchema schema, string targetJson, out IList<string> errors) {
     return JObject.Parse(targetJson).IsValid(schema, out errors);
   }
 
@@ -45,11 +45,11 @@ public static class SchemaHelper {
   /// Determines whatever the JSON file is valid against the Schema JSON
   /// </summary>
   /// <param name="schema">The schema to test with</param>
-  /// <param name="jsonFile">Absolute json file path</param>
+  /// <param name="jsonFile">Absolute JSON file path</param>
   /// <param name="errors">When this method returns, contains any error messages generated while validating.</param>
   /// <returns>True when no errors, false otherwise</returns>
-  public static bool ValidateFile(string schema, string jsonFile, out IList<string> errors) {
+  public static bool TryParseFile(string schema, string jsonFile, out IList<string> errors) {
     var jsonData = File.ReadAllText(jsonFile);
-    return ValidateText(schema, jsonData, out errors);
+    return TryParse(schema, jsonData, out errors);
   }
 }
