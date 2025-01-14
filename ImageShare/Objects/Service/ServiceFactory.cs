579ebd7b62a9a1b727111891164ea11ec4c7debb
Junaid Atari <mj.atari@gmail.com>
2025-01-14 19:07:04 +0500

    change class reference

--- ImageShare/Objects/Service/ServiceFactory.cs
+++ ImageShare/Objects/Service/ServiceFactory.cs
@@ -1,6 +1,11 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2024-2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
 using System.IO;
 using Newtonsoft.Json;
 using PixPost.Helpers;
+using PixPost.Objects.Service.Objects;
 
 namespace PixPost.Objects.Service;
 
