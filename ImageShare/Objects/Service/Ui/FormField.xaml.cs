63e4f3b771f90be2602ca0236a7727f598acbda4
Junaid Atari <mj.atari@gmail.com>
2025-01-15 00:39:51 +0500

    create custom user control

---
+++ ImageShare/Objects/Service/Ui/FormField.xaml.cs
@@ -0,0 +1,25 @@
+using System.Windows;
+using Dumpify;
+using PixPost.Objects.Service.Objects;
+
+namespace PixPost.Objects.Service.Ui;
+
+using SchemaSpecs = SchemaSpecs;
+
+public partial class FormField {
+  public static readonly DependencyProperty ItemSourceProperty = DependencyProperty.Register(
+    nameof(ItemSource), typeof(SchemaSpecs.Variable), typeof(FormField),
+    new PropertyMetadata(default(SchemaSpecs.Variable), (o, args) => {
+      Console.WriteLine("XXX");
+      ((o as FormField)!).ItemSource = (SchemaSpecs.Variable)args.NewValue;
+    }));
+
+  public SchemaSpecs.Variable ItemSource {
+    get => (SchemaSpecs.Variable)GetValue(ItemSourceProperty);
+    set => SetValue(ItemSourceProperty, value);
+  }
+
+  public FormField() {
+    InitializeComponent();
+  }
+}
