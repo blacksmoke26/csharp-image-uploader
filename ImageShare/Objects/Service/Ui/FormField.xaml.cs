d08d538da07ab87cb095c3105b115b6986aa4f81
Junaid Atari <mj.atari@gmail.com>
2025-01-16 00:16:36 +0500

    implement proper data and context binding

--- ImageShare/Objects/Service/Ui/FormField.xaml.cs
+++ ImageShare/Objects/Service/Ui/FormField.xaml.cs
@@ -1,23 +1,26 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2024-2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
 using System.Windows;
 using System.Windows.Controls;
 using PixPost.Objects.Service.Objects;
 
 namespace PixPost.Objects.Service.Ui;
 
-using SchemaSpecs = SchemaSpecs;
-
 public partial class FormField {
-  public static readonly DependencyProperty ItemSourceProperty = DependencyProperty.Register(
-    nameof(ItemSource), typeof(SchemaSpecs.Variable), typeof(FormField),
+  public static readonly DependencyProperty SchemaProperty = DependencyProperty.Register(
+    nameof(Schema), typeof(FormFieldInfo), typeof(FormField),
     new PropertyMetadata(null, (o, args) => {
       var obj = (FormField)o;
-      obj.ItemSource = (SchemaSpecs.Variable)args.NewValue;
+      obj.Schema = (FormFieldInfo)args.NewValue;
+      obj.DataContext = args.NewValue;
       obj.InitializeInputs();
     }));
 
-  public SchemaSpecs.Variable ItemSource {
-    get => (SchemaSpecs.Variable)GetValue(ItemSourceProperty);
-    set => SetValue(ItemSourceProperty, value);
+  public FormFieldInfo Schema {
+    get => (FormFieldInfo)GetValue(SchemaProperty);
+    set => SetValue(SchemaProperty, value);
   }
 
   public FormField() {
@@ -25,14 +28,12 @@ public partial class FormField {
   }
 
   public void InitializeInputs() {
-    var container =  MainGrid.Children;
-    var templateControl = ItemSource.InputField.Type switch {
-      SchemaSpecs.InputFieldType.Toggle => Resources["ControlCheckbox"],
-      SchemaSpecs.InputFieldType.List => Resources["ControlComboBox"],
+    var templateControl = Schema.Type switch {
+      InputFieldType.Toggle => Resources["ControlCheckbox"],
+      InputFieldType.List => Resources["ControlComboBox"],
       _ => Resources["ControlText"],
     };
 
-    var control = (ContentControl)templateControl!;
-    container.Add(control);
+    FieldContentControl.Content = (ContentControl)templateControl!;
   }
 }
