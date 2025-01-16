d4f154e83858605da8dfb303b7af713e4965a1c1
Junaid Atari <mj.atari@gmail.com>
2025-01-16 20:15:26 +0500

    create control-templates, implement logic

--- ImageShare/Objects/Service/Ui/FormField.xaml.cs
+++ ImageShare/Objects/Service/Ui/FormField.xaml.cs
@@ -28,12 +28,83 @@ public partial class FormField {
   }
 
   public void InitializeInputs() {
-    var templateControl = Schema.Type switch {
-      InputFieldType.Toggle => Resources["ControlCheckbox"],
-      InputFieldType.List => Resources["ControlComboBox"],
-      _ => Resources["ControlText"],
-    };
+    if (Schema.Type switch {
+          InputFieldType.Toggle => Resources["ControlToggle"]!,
+          InputFieldType.List => Resources["ControlComboBox"]!,
+          _ => Resources["ControlText"]!,
+        } is not ContentControl templateControl) {
+      return;
+    }
 
-    FieldContentControl.Content = (ContentControl)templateControl!;
+    foreach (var child in LogicalTreeHelper.GetChildren(templateControl)) {
+      if (Schema.Type == InputFieldType.Double)
+        InitDoubleControl((TextBox)child);
+      else if (Schema.Type == InputFieldType.Url)
+        InitUrlControl((TextBox)child);
+      else if (Schema.Type == InputFieldType.Toggle)
+        InitToggleControl((CheckBox)child);
+      else if (Schema.Type == InputFieldType.Integer)
+        InitIntegerControl((TextBox)child);
+      else if (Schema.Type == InputFieldType.List)
+        InitListControl((ComboBox)child);
+      else
+        InitTextControl((TextBox)child);
+    }
+
+    FieldContentControl.Content = templateControl;
+  }
+
+  private void InitTextControl(TextBox control) {
+    if (Schema.MaxLength != null)
+      control.MaxLength = (int)Schema.MaxLength;
+    if (Schema.ExactLength != null)
+      control.MaxLength = (int)Schema.ExactLength;
+  }
+
+  private void InitDoubleControl(TextBox control) {
+    control.PreviewTextInput += (sender, e) => {
+      /*
+      var isNum = e.Text == "." || int.TryParse(ctrl.Text, out _) || decimal.TryParse(ctrl.Text, out _);
+        Console.WriteLine(isNum);
+        e.Handled = r.IsMatch(ctrl.Text);//!isNum || !decimal.TryParse(e.Text, out var _));*/
+      //e.Handled = !e.Text.All(cc => Char.IsNumber(cc) || cc == '.' || cc == '-');
+      //ctrl.Text = Regex.Replace(ctrl.Text,  @"[^\d\.]+", "");
+      //base.OnPreviewTextInput(e);
+    };
+    /*control.PreviewKeyDown += (sender, e) => {
+      var ctrl = (TextBox)sender;
+      ctrl.Text = e.Key switch {
+        Key.Up when double.TryParse(ctrl.Text, out var uv) => $"{uv + 1}",
+        Key.Down when double.TryParse(ctrl.Text, out var dv) => $"{dv - 1}",
+        _ => ctrl.Text
+      };
+    };
+    control.MouseWheel += (sender, e) => {
+      var ctrl = (TextBox)sender;
+      ctrl.Text = e.Delta switch {
+        > 0 => $"{double.Parse(ctrl.Text) + 1}",
+        < 0 => $"{double.Parse(ctrl.Text) - 1}",
+        _ => ctrl.Text
+      };
+    };*/
+    /*DataObject.AddPastingHandler(control, (sender, e) => {
+      if (e.DataObject.GetDataPresent(typeof(string))) {
+        var text = (string)e.DataObject.GetData(typeof(string))!;
+        if (!double.TryParse(text, out _)) e.CancelCommand();
+      }
+      else e.CancelCommand();
+    });*/
+  }
+
+  private void InitUrlControl(TextBox control) {
+  }
+
+  private void InitToggleControl(CheckBox control) {
+  }
+
+  private void InitIntegerControl(TextBox control) {
+  }
+
+  private void InitListControl(ComboBox control) {
   }
 }
